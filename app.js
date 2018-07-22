4ab50318c60ea2f66225bcd571f77bb76c44544d
Unknown <info@aviator-coding.de>
2018-07-21 22:24:54 -0400

    Changed Template Engine Jade -> Pug
    changed the Entire Template Engine to the Latest Pug Version.

--- app.js
+++ app.js
@@ -71,7 +71,7 @@ i18next
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'pug');
 app.use(i18nextMiddleware.handle(i18next));
 
 app.use(favicon(path.join(__dirname, settings.favicon)));
